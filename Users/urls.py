b275bf62fce539b27133850a6db973fb3831db8d
arun <arunvtsn@gmail.com>
2022-01-18 14:42:34 +0530

    final commit

--- Users/urls.py
+++ Users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from Users import views
-from rest_framework.authtoken.views import obtain_auth_token
+#from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
@@ -9,7 +9,7 @@ from .views import ChangePasswordView
 
 urlpatterns = [
     path('register/', views.register.as_view(),name='register'),
-    path('login/',obtain_auth_token,name='login'),
+    #path('login/',obtain_auth_token,name='login'),
     path('hello/', views.HelloView.as_view(),name='hello'),
     path('api/token/',jwt_views.TokenObtainPairView.as_view(),name ='token_obtain_pair'),
     path('api/token/refresh/',jwt_views.TokenRefreshView.as_view(),name ='token_refresh'),
